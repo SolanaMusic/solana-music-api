47843cbb4b1bbabd32a3c12c4a2c4dfe8b18ca31
Vladyslav Linnik <vlad.linnik555@gmail.com>
2025-02-12 02:17:48 +0200

    exception middleware added

---
+++ solana-music-api/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace solana_music_api;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var response = new
+        {
+            Error = exception.Message,
+            Trace = exception.StackTrace
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        return context.Response.WriteAsJsonAsync(response);
+    }
+}
--- solana-music-api/Program.cs
+++ solana-music-api/Program.cs
@@ -30,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 await app.CreateDefaultRolesAsync();
 await app.CreateDefaultUsersAsync();
 
